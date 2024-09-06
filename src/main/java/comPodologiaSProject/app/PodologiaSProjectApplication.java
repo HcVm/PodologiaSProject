63c0c5d79111dbe83972bc272a101927418387f1
HcVm <hecvega96@outlook.com>
2024-09-05 23:19:08 -0500

    Inicial

---
+++ src/main/java/comPodologiaSProject/app/PodologiaSProjectApplication.java
@@ -0,0 +1,13 @@
+package comPodologiaSProject.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodologiaSProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodologiaSProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comPodologiaSProject/app/PodologiaSProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package comPodologiaSProject.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodologiaSProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
